5db8160bbbd046d91632a6962ead28997116d339
Richard <rickytaylor49@gmail.com>
2016-07-14 18:17:30 +0100

    basic conversion start from fox and rabbit program used in chapter 10 of the Adv OOP lessons

--- RoombaMap/Map-Test/src/Main.java
+++ RoombaMap/Map-Test/src/Main.java
@@ -1,9 +1,13 @@
 
 public class Main {
 
+	private static SimulatorView sim;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		System.out.println("hi");
+		
+		sim = new SimulatorView(100, 100);
 
 	}
 
