a15797aeb6a025ba1a09269373221d9afb5f5b58
Richard <rickytaylor49@gmail.com>
2016-08-08 21:13:44 +0100

    working coms!

--- RoombaMap/Map-Test/src/Communication.java
+++ RoombaMap/Map-Test/src/Communication.java
@@ -2,43 +2,27 @@
 
 public class Communication {
 
-	private SerialListen sl;
-	private SerialWrite sw;
+	
 	
 	public Communication(){
-		sl = new SerialListen();
-		sw = new SerialWrite();
+		
 	}
 	
 	public String listenForMessage(){
+	
+		SimpleRead sr = new SimpleRead();
+		sr.read();
 		
+		String message = "" + sr.getR();
+		return message;
 		
-		try {
-			System.out.println("listening");
-			sl.listen();
-			String message = sl.getLastMessage();
-			boolean waiting = true;
-			while (waiting){
-			//System.out.println("waiting... com message recived: " + message);
-			if(message != null){
-				waiting = false;
-				return message;	
-				
-		} 
-			}
-			}catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				
-			}
-		return null;
 	}
 	
 	
 	
 	
 	public void writeMessage(String message){
-		
+		SimpleWrite sw = new SimpleWrite();
 		sw.write(message);
 		sw.write(".");
 		
